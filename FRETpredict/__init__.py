308992b1ffd77899ba3a60062236eed4977a9b7b
Daniele Montepietra <59091549+Monte95@users.noreply.github.com>
2022-05-11 08:47:04 +0200

    Update __init__.py

--- FRETpredict/__init__.py
+++ FRETpredict/__init__.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+from .FRET import FRETpredict
+from .rotamer_libraries import RotamerClusters
+from .utils import Operations
+from .lennardjones import lj_parameters
+from .libraries import *
+
 
 __version__ = '0.1.0'
 
