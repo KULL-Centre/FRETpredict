c0383ca0eba31abd8ed589a0e61f828fdb7beea0
Daniele Montepietra <59091549+Monte95@users.noreply.github.com>
2022-05-12 09:44:41 +0200

    Updated test_Hsp90 and pytest.ini

--- tests/test_Hsp90.py
+++ tests/test_Hsp90.py
@@ -1,5 +1,6 @@
 import pytest
 import MDAnalysis
+import pandas as pd
 import numpy as np
 from FRETpredict import FRETpredict
 
@@ -7,89 +8,47 @@ from FRETpredict import FRETpredict
 def test_Hsp90_R0_calculation():
     
     # Import protein structure
-    u = MDAnalysis.Universe('../test_systems/Hsp90/openHsp90.pdb')
+    u = MDAnalysis.Universe('../FRETpredict/test_systems/Hsp90/openHsp90.pdb')
     
     # Instantiate class object
     FRET = FRETpredict(protein=u, residues=[452, 637], temperature=293, 
                    chains=['A', 'B'], 
                    donor='AlexaFluor 594', acceptor='AlexaFluor 568', 
-                   electrostatic=True, r0lib='../lib/R0/',
-                   libname_1=f'AlexaFluor 594 C1R cutoff30',
-                   libname_2=f'AlexaFluor 568 C1R cutoff30', 
-                   output_prefix=f'data/E30')
+                   electrostatic=True, r0lib='../FRETpredict/lib/R0/',
+                   libname_1='AlexaFluor 594 C1R cutoff30',
+                   libname_2='AlexaFluor 568 C1R cutoff30', 
+                   output_prefix='data/E30')
     
     # Run FRETpredict calculations
-    assert FRET.run(), "Cannot calculate FRET Efficiency with explicit R0 calculation"
+    FRET.run()
+#     "Cannot calculate FRET Efficiency with explicit R0 calculation"
     
     # Read computed data from file
-    assert pd.read_pickle(r'data/E30-data-594-568.pkl'), 
+    assert pd.read_pickle('data/E30-data-452-637.pkl')['Estatic'] != np.nan,  \
     "Could not read FRET data from file (explicit R0 calculations)"
 
 # Test with single frame, fixed R0 value
 def test_Hsp90_fixed_R0():
     
     # Import protein structure
-    u = MDAnalysis.Universe('../test_systems/Hsp90/openHsp90.pdb')
+    u = MDAnalysis.Universe('../FRETpredict/test_systems/Hsp90/openHsp90.pdb')
     
     # Instantiate class object
     FRET_fixedR0 = FRETpredict(protein=u, residues=[452, 637], temperature=293, 
                            chains=['A', 'B'], 
                            fixed_R0=True, r0=5.5, electrostatic=True,
-                           libname_1=f'AlexaFluor 594 C1R cutoff30',
-                           libname_2=f'AlexaFluor 568 C1R cutoff30', 
-                           output_prefix=f'data/E30_fixedR0')
+                           libname_1='AlexaFluor 594 C1R cutoff30',
+                           libname_2='AlexaFluor 568 C1R cutoff30', 
+                           output_prefix='data/E30_fixedR0')
     
     # Run FRETpredict calculations
-    assert FRET.run(), "Cannot calculate FRET Efficiency with fixed R0"
+    FRET_fixedR0.run()
+#     "Cannot calculate FRET Efficiency with fixed R0"
     
     # Read computed data from file
-    assert pd.read_pickle(r'data/E30_fixedR0-data-594-568.pkl'), 
+    assert pd.read_pickle('data/E30_fixedR0-data-452-637.pkl')['Estatic'] != np.nan,  \
     "Could not read FRET data from file (fixed R0)"
 
-# Test with trajectory, explicit R0 calculations
-def test_Hsp90_trajectory_R0_calculation():
-    
-    # Import protein trajectory
-    u = MDAnalysis.Universe('../test_systems/Hsp90/conf.pdb', 
-                            '../test_systems/Hsp90/Hsp90_open_all.xtc')
-    
-    # Instantiate class object
-    FRET_traj = FRETpredict(protein=u, residues=[452, 637], temperature=293, 
-                        chains=['A', 'B'], 
-                        donor='AlexaFluor 594', acceptor='AlexaFluor 568', 
-                        electrostatic=True, r0lib='../lib/R0/',
-                        libname_1=f'AlexaFluor 594 C1R cutoff30',
-                        libname_2=f'AlexaFluor 568 C1R cutoff30', 
-                        output_prefix=f'data/E30_traj')
-    
-    # Run FRETpredict calculations
-    assert FRET.run(), "Cannot calculate FRET Efficiency for trajectory with explicit R0 calculation"
-    
-    # Read computed data from file
-    assert pd.read_pickle(r'data/E30_traj-data-594-568.pkl'), 
-    "Could not read FRET data from file (trajectory with explicit R0 calculations)"
-
-# Test with trajectory, fixed R0 value
-def test_Hsp90_trajectory_fixed_R0():
-    
-    # Import protein trajectory
-    u = MDAnalysis.Universe('../test_systems/Hsp90/conf.pdb', 
-                            '../test_systems/Hsp90/Hsp90_open_all.xtc')
-    
-    # Instantiate class object
-    FRET_traj = FRETpredict(protein=u, residues=[452, 637], temperature=293, 
-                        chains=['A', 'B'], 
-                        fixed_R0=True, r0=5.5, electrostatic=True,
-                        libname_1=f'AlexaFluor 594 C1R cutoff30',
-                        libname_2=f'AlexaFluor 568 C1R cutoff30', 
-                        output_prefix=f'data/E30_traj_fixedR0')
-    
-    # Run FRETpredict calculations
-    assert FRET.run(), "Cannot calculate FRET Efficiency for trajectory with fixed R0"
-    
-    # Read computed data from file
-    assert pd.read_pickle(r'data/E30_traj_fixedR0-data-594-568.pkl'), 
-    "Could not read FRET data from file (trajectory with fixed R0)"
     
     
     
